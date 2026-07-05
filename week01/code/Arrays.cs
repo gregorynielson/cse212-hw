bfd47933f22da3bc9d33b5ff8c003513ec59735e
gregorynielson <gnielson97@gmail.com>
2026-07-05 10:39:18 -0400

    week 1 assignments

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,35 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //new array variable to return at end
+        double[] multipleArray = new double[length];
+
+        //new variable to hold multipliers in loop, starts at 1, named 'newMultiple'
+        double newMultiple = 1;
+
+        //new variable to act as loop counter named 'loopCount'
+        int loopCount = 1;
+
+        //while loop that ends when 'length' variable is reached
+        while (loopCount <= length)
+        {
+
+            //inside loop, start with 1 multiplied by 'number'
+            //inside loop, replace 'newMultiple' for next time through loop
+            newMultiple = loopCount * number;
+
+            //get index
+            int index = loopCount - 1;
+
+            //inside loop, add newMultiple to array
+            multipleArray[index] = newMultiple;
+
+            //inside loop, add 1 to 'loopCount'
+            loopCount++;
+
+        }
+
+        return multipleArray; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +57,29 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //add counter to iterate loop until 'amount' is reached
+        int loopCount = 1;
+
+        //get index of last value in array
+        int lastIndex = data.Count - 1;
+
+        //while loop, iterates until 'amount' is reached
+        while (loopCount <= amount)
+        {
+
+            //get value of last in array
+            int value = data[lastIndex];
+
+            //remove last value
+            data.RemoveAt(lastIndex);
+
+            //add the value that was just removed to beginning of array
+            data.Insert(0, value);
+
+            //add to loop counter
+            loopCount++;
+
+        }
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,30 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var l3 = new int[select.Length];
+        int listOneIndex = 0;
+        int listTwoIndex = 0;
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                l3[i] = list1[listOneIndex];
+                listOneIndex++;
+            }
+            else
+            {
+                l3[i] = list2[listTwoIndex];
+                listTwoIndex++;
+            }
+        }
+        return l3;
     }
 }
