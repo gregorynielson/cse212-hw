678512e80e2a85bc4fc5261e83e6de58795bafa2
gregorynielson <gnielson97@gmail.com>
2026-07-11 12:12:38 -0400

    week 2 assignment

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -24,16 +24,19 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 0; index < _queue.Count - 1; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
+
+    public int Count => _queue.Count;
 
     // DO NOT MODIFY THE CODE IN THIS METHOD
     // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
 
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
 
@@ -6,23 +7,119 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with the following value and priority: Serious (2), Critical (3), Minor (1) and run
+    // until queue is empty.
+    // Expected Result: Critical, Serious, Minor
+    // Defect(s) Found: Items were not being removed from the queue when Dequeue() was called
     public void TestPriorityQueue_1()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var serious = new PriorityItem("Serious", 2);
+        var critical = new PriorityItem("Critical", 3);
+        var minor = new PriorityItem("Minor", 1);
+
+        PriorityItem[] expectedResult = [critical, serious, minor];
+
+        PriorityQueue priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue(serious.Value, serious.Priority);
+        priorityQueue.Enqueue(critical.Value, critical.Priority);
+        priorityQueue.Enqueue(minor.Value, minor.Priority);
+
+
+        int i = 0;
+
+        while (priorityQueue.Count > 0)
+        {
+            if (i > expectedResult.Length)
+            {
+                Assert.Fail("Queue should have ran out of items by now.");
+            }
+
+            var item = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, item);
+            i++;
+        }
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with the following value and priority: Minor (1), Critical (3), Important (2), Imperative (3), Serious (2), Noteworthy (1)
+    // and run until the queue is empty
+    // Expected Result: Critical, Imperative, Important, Serious, Minor, Noteworthy
+    // Defect(s) Found: Code was set up to take the last item in the queue with the highest priority instead of the first item in the queue
+    // with the highest priority
     public void TestPriorityQueue_2()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var minor = new PriorityItem("Minor", 1);
+        var critical = new PriorityItem("Critical", 3);
+        var important = new PriorityItem("Important", 2);
+        var imperative = new PriorityItem("Imperative", 3);
+        var serious = new PriorityItem("Serious", 2);
+        var noteworthy = new PriorityItem("Noteworthy", 1);
+
+        PriorityItem[] expectedResult = [critical, imperative, important, serious, minor, noteworthy];
+
+        PriorityQueue priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue(minor.Value, minor.Priority);
+        priorityQueue.Enqueue(critical.Value, critical.Priority);
+        priorityQueue.Enqueue(important.Value, important.Priority);
+        priorityQueue.Enqueue(imperative.Value, imperative.Priority);
+        priorityQueue.Enqueue(serious.Value, serious.Priority);
+        priorityQueue.Enqueue(noteworthy.Value, noteworthy.Priority);
+
+
+
+        int i = 0;
+
+        while (priorityQueue.Count > 0)
+        {
+            if (i > expectedResult.Length)
+            {
+                Assert.Fail("Queue should have ran out of items by now.");
+            }
+
+            var item = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, item);
+            i++;
+        }
+
+    }
+
+
+    [TestMethod]
+    // Scenario: Create a queue with the following value and priority: Math (3), English (3), History(3), Science (3) and run
+    // until queue is empty to test whether items are added to the queue correctly.
+    // Expected Result: Math, English, History, Science
+    // Defect(s) Found: 
+    public void TestPriorityQueue_3()
+    {
+        var math = new PriorityItem("Math", 3);
+        var english = new PriorityItem("English", 3);
+        var history = new PriorityItem("History", 3);
+        var science = new PriorityItem("Science", 3);
+
+        PriorityItem[] expectedResult = [math, english, history, science];
+
+        PriorityQueue priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue(math.Value, math.Priority);
+        priorityQueue.Enqueue(english.Value, english.Priority);
+        priorityQueue.Enqueue(history.Value, history.Priority);
+        priorityQueue.Enqueue(science.Value, science.Priority);
+
+
+        int i = 0;
+
+        while (priorityQueue.Count > 0)
+        {
+            if (i > expectedResult.Length)
+            {
+                Assert.Fail("Queue should have ran out of items by now.");
+            }
+
+            var item = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, item);
+            i++;
+        }
+
     }
 
     // Add more test cases as needed below.
