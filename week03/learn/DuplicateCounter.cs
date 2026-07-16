41f468fc009dd84e57134d5dee371042792ab3c3
gregorynielson <gnielson97@gmail.com>
2026-07-16 16:38:30 -0400

    week 3 learning activities

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -25,6 +25,18 @@
     private static int CountDuplicates(int[] data)
     {
         // Add code here.
-        return 0;
+        HashSet<int> duplicates = new HashSet<int>();
+        int count = 0;
+
+        foreach (int number in data) {
+            if (duplicates.Contains(number)) {
+                count += 1;
+            }
+            else {
+                duplicates.Add(number);
+            }
+        }
+
+        return count;
     }
 }
